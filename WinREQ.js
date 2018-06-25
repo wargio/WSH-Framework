9934dd22b62a1f6b25b0421c1a4c6ac84603d07e
Giovanni <wargio@libero.it>
2018-06-25 10:29:28 +0200

    Update WinREQ.js

--- WinREQ.js
+++ WinREQ.js
@@ -15,6 +15,36 @@ if (typeof WMI == 'undefined')
 if (typeof Options == 'undefined')
     console.require('Options.js');
 
+
+var print_object = function(who) {
+    var max = 0,
+        pad = '                                         ';
+    for (var entry in who) {
+        if (who[entry] && max < entry.length)
+            max = entry.length;
+    }
+    for (var entry in who) {
+        if (who[entry])
+            console.log("    " + entry + ": " + pad.substr(0, max - entry.length) + who[entry]);
+    }
+    console.log(" ");
+}
+
+var toUnit = function(n) {
+    if (typeof n == 'string') {
+        n = parseInt(n);
+    }
+    if (n < Math.pow(1024, 4)) {
+        return (n/Math.pow(1024, 3)).toFixed(0) + ' GB';
+    } else if (n < Math.pow(1024, 3)) {
+        return (n/Math.pow(1024, 2)).toFixed(0) + ' MB';
+    } else if (n < Math.pow(1024, 2)) {
+        return (n/1024).toFixed(0) + ' KB';
+    } else {
+        return n + ' bytes';
+    }
+}
+
 var opts = {
     '--all': {
         args: 0,
@@ -48,6 +78,41 @@ var opts = {
             wmi.print();
         },
         usage: "[value]        | shows a specific object."
+    },
+    '--info': {
+        args: 0,
+        short: '-i',
+        exec: function(args) {
+            var info = {
+                'Serial Number': '',
+                'Manufacturer': '',
+                'Model': '',
+                'OS': '',
+                'RAM': '',
+                'HDD': '',
+                'CPU': '',
+                'Display': ''
+            };
+            var wmi = new WMI();
+            wmi.load("BIOS");
+            wmi.load("OperatingSystem");
+            wmi.load("PhysicalMemory");
+            wmi.load("ComputerSystem");
+            wmi.load("DiskDrive");
+            wmi.load("DesktopMonitor");
+            wmi.load("Processor");
+            info['Serial Number'] = wmi.BIOS[0].serialNumber;
+            info.OS = wmi.OperatingSystem[0].caption;
+            info.RAM = toUnit(wmi.PhysicalMemory[0].capacity) + ' (' + wmi.PhysicalMemory[0].speed + ' MHz)';
+            info.Manufacturer = wmi.ComputerSystem[0].manufacturer;
+            info.Model = wmi.ComputerSystem[0].model;
+            info.HDD = toUnit(wmi.DiskDrive[0].size);
+            info.CPU = wmi.Processor[0].name;
+            info.Display = wmi.DesktopMonitor[0].screenWidth + 'x' + wmi.DesktopMonitor[0].screenHeight + ' pixels';
+            console.log('INFO:')
+            print_object(info);
+        },
+        usage: "               | shows all the infos about the PC."
     }
 };
 
